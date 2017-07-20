744c4aa257b2e8fbafea060acd91d4aa437d2e98
adskyfly <466703496@qq.com>
2017-07-20 17:05:10 +0800

    vue-music

---
+++ src/base/scroll/scroll.vue
@@ -0,0 +1,66 @@
+<template>
+  <div ref="wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  import BScroll from 'better-scroll'
+  export default{
+    props:{
+      probeType: {
+      	type: Number,
+        default: 1
+      },
+      click: {
+      	type: Boolean,
+        default: true
+      },
+      data: {
+        type: Array,
+        default: null
+      },
+      listenScroll: {
+      	type: Boolean,
+        default: false
+      }
+    },
+    mounted(){
+      setTimeout(() => {
+      	this._initScroll()
+      },20)
+    },
+    watch:{
+    	data(){
+    		setTimeout(()=>{
+    			this.refresh()
+        },20)
+      }
+    },
+    methods: {
+    	_initScroll(){
+        if(!this.$refs.wrapper)return
+
+        this.scroll = new BScroll(this.$refs.wrapper, {
+        	probeType: this.probeType,
+          click: this.click,
+        })
+      },
+      scrollTo(){
+        this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
+      },
+      scrollToElement(){
+        this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments)
+      },
+      enable(){
+        this.scroll && this.scroll.enable()
+      },
+      disable(){
+      	this.scroll && this.scroll.disable()
+      },
+      refresh(){
+        this.scroll && this.scroll.refresh()
+      }
+    }
+  }
+</script>
